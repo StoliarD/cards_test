19348fb8a9ab2d8ee8104981bd21710e31e4a7f7
stoliard <stoliard239@gmail.com>
2018-05-07 01:23:38 +0300

    forgot about this

--- src/test/java/ru/cards_mobile/test/core/MagicTest.java
+++ src/test/java/ru/cards_mobile/test/core/MagicTest.java
@@ -1,5 +1,7 @@
 package ru.cards_mobile.test.core;
 
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import ru.cards_mobile.test.core.events.CounterEvent;
 
@@ -7,21 +9,31 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 public class MagicTest {
 
-    @Test
-    public void test() {
-        AtomicInteger count = new AtomicInteger();
-        MagicService service = new MagicService() {
-            @Override
-            void magicMethod() throws Throwable {
-                count.incrementAndGet();
-                Thread.sleep(1000);
+    private AtomicInteger integer = new AtomicInteger();
+    private MagicService service;
 
+    @Before
+    public void before() {
+        service = new MagicService() {
+            @Override
+            void magicMethod() throws Throwable { // when invoked fist time - imitates events spam while job is running
+                integer.incrementAndGet();
+                if (integer.get() == 1) {
+                    service.onApplicationEvent(new CounterEvent(this));
+                    service.onApplicationEvent(new CounterEvent(this));
+                    service.onApplicationEvent(new CounterEvent(this));
+                }
+                Thread.sleep(100);
             }
         };
+        service.init();
+    }
 
+    @Test
+    public void test() throws InterruptedException {
         service.onApplicationEvent(new CounterEvent(this));
-
-
+        Thread.sleep(3000); //because of executor delay
+        Assert.assertEquals(2, integer.get());
     }
 
 
